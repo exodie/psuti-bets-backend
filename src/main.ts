24225c2690ffe36435832c8cc91f844d7c82b063
mitrofanov <83586607+exodie@users.noreply.github.com>
2023-12-22 19:23:34 +0400

    update [0.1.1] backend server
    - Удалил ненужные файлы из /src
    - Добавил Swagger UI в качестве документации
    - Добавил .env.example в качестве основного .env файла (сделать пометку в README)
    - Текущая версия: 0.1.1

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { config } from 'dotenv';
+
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Bets')
+    .setDescription('The docs for bets API')
+    .setVersion('0.1.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app
+    .listen(process.env.PORT)
+    .then(
+      () =>
+        new Logger.log('Listen', `Application use ${process.env.PORT} port.`),
+    );
 }
 bootstrap();
