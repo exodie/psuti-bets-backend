924555ff1afdc063bc7f2062248d69f8b8b2dfa9
mitrofanov <83586607+exodie@users.noreply.github.com>
2023-12-31 14:00:04 +0500

    add matches entity
    - Добавлены базовые компоненты для матчей.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,12 @@ import { config } from 'dotenv';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { UsersModule } from './users/users.module';
+import { MatchesModule } from './matches/matches.module';
 
 config();
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.DATABASE_URI), UsersModule],
+  imports: [MongooseModule.forRoot(process.env.DATABASE_URI), UsersModule, MatchesModule],
   controllers: [],
   providers: [],
 })
